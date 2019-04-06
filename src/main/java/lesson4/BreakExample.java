b78469439e17626b3c35aa26fd4225f651c36a4f
s.nedaikhleb <s.nedaikhleb@hexa.com.ua>
2019-04-06 17:13:44 +0300

    homework 3 part 1

---
+++ src/main/java/lesson4/BreakExample.java
@@ -0,0 +1,10 @@
+package lesson4;
+
+public class BreakExample {
+
+    public static void main(String[] args) {
+        for (int a=1; a<=10; a++){
+
+        }
+    }
+}
---
+++ src/main/java/lesson4/ForExample.java
@@ -0,0 +1,16 @@
+
+package lesson4;
+
+public class ForExample {
+
+    public static void main(String[] args) {
+
+        for(int i=1;i <=100;i++) {
+            System.out.println(i + "");
+        }
+
+        for(int i=10;i >=-11;i--) {
+            System.out.println(i + "");
+        }
+    }
+}
