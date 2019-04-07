e0401dc4eb8dee759d848842eaa863ef89444834
s.nedaikhleb <s.nedaikhleb@hexa.com.ua>
2019-04-07 20:40:48 +0300

    lesson 4

--- src/main/java/lesson4/DaysOfWeek.java
+++ src/main/java/lesson4/DaysOfWeek.java
@@ -1,10 +1,33 @@
 package lesson4;
 
+import java.util.Scanner;
+
 public class DaysOfWeek {
 
     public static void main(String[] args) {
 
-
+        Scanner in = new Scanner(System.in);
+        int day = in.nextInt();
+        String dayString;
+        switch (day) {
+            case 1:  dayString = "Monday";
+                break;
+            case 2:  dayString = "Tuesday";
+                break;
+            case 3:  dayString = "Wednesday";
+                break;
+            case 4:  dayString = "Thursday";
+                break;
+            case 5:  dayString = "Friday";
+                break;
+            case 6:  dayString = "Saturday";
+                break;
+            case 7:  dayString = "Sunday";
+                break;
+            default: dayString = "I don't know this day";
+                break;
+        }
+        System.out.print(dayString);
     }
 
     }
