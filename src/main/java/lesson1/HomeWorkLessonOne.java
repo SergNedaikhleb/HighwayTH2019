a3482bc910b6069e343bb3750e2d24780e28f9b2
s.nedaikhleb <s.nedaikhleb@hexa.com.ua>
2019-04-08 19:40:37 +0300

    lesson

--- src/main/java/lesson1/HomeWorkLessonOne.java
+++ src/main/java/lesson1/HomeWorkLessonOne.java
@@ -2,9 +2,12 @@ package lesson1;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class HomeWorkLessonOne {
@@ -13,8 +16,13 @@ public class HomeWorkLessonOne {
 
         System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
 
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("profile.default_content_setting_values.notifications", 2);
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", prefs);
+        WebDriver driver = new ChromeDriver(options);
 
-        WebDriver driver = new ChromeDriver();
+
         driver.manage().window().maximize();
         driver.get("https://www.facebook.com/");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -23,18 +31,18 @@ public class HomeWorkLessonOne {
         WebElement emailField = driver.findElement(By.id("email"));
         emailField.sendKeys("neeshloshad@gmail.com");
         WebElement passwordField = driver.findElement(By.id("pass"));
-        passwordField.sendKeys("***********"); //sorry, but I can't write you my password
+        passwordField.sendKeys("********"); //sorry, but I can't write you my password
         WebElement loginButton = driver.findElement(By.id("u_0_3"));
         loginButton.submit();
 
-        WebElement waitFor = (new WebDriverWait(driver, 10))
+        WebElement waitFor = (new WebDriverWait(driver, 8))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.id("u_0_c")));
 
-        WebElement messages = driver.findElement(By.id("js_55"));
+        WebElement messages = driver.findElement(By.xpath("//*[@id='u_0_d']/a/div"));
         messages.click();
 
-        WebElement waitForMenu = (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.visibilityOfElementLocated(By.id("messagesContent")));
+        WebElement waitForMenu = (new WebDriverWait(driver, 5))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.className("messagesContent")));
 
         System.out.println("My last Message is: " + driver.findElement(By.className("messagesContent")).getText());
 
