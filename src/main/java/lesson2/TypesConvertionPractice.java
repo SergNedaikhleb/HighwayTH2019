80657ce4f417bb48e90337f77d6bff7303e4a0ae
s.nedaikhleb <s.nedaikhleb@hexa.com.ua>
2019-04-09 15:14:17 +0300

    lesson fix hometask 2

--- src/main/java/lesson2/TypesConvertionPractice.java
+++ src/main/java/lesson2/TypesConvertionPractice.java
@@ -4,17 +4,17 @@ public class TypesConvertionPractice {
 
     public static void main(String[] args) {
 
-        int intToCharValue = 9;
+        int intToCharValue = 918;
         char x;
         x = (char) intToCharValue;
 
-        System.out.println(intToCharValue);
+        System.out.println(x);
 
-        float floatToInt = 1.2345F;
+        float floatToInt = 1.23F;
         char charValue;
         charValue = (char) floatToInt;
 
-        System.out.println(floatToInt);
+        System.out.println(charValue);
 
         float floatToChar = 9.8765F;
         char thenToInt = (char) floatToChar;
