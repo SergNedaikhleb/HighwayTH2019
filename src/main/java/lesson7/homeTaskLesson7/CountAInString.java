6e279d1c981b92d4e2be8e233d487279dc0f4890
s.nedaikhleb <s.nedaikhleb@hexa.com.ua>
2019-05-09 16:55:50 +0300

    hometask 7

--- src/main/java/lesson7/homeTaskLesson7/CountAInString.java
+++ src/main/java/lesson7/homeTaskLesson7/CountAInString.java
@@ -1,15 +1,21 @@
 package lesson7.homeTaskLesson7;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class CountAInString {
     public static void main(String[] args) {
-        String necessaryString =
-                "aasss,assdfaasdqweaaa,asdaaaas,adsasdasd,aaa,asdghnzzzzzza,zzzzxxxaaaa,zxvvxvxcvxcv,xcvxv.fsdfsdfeweqwejhsdf sf sdfs a a sdfsdf sdf";
-        String necessaryLetter ="a";
-        String []letterArray = necessaryString.split(necessaryLetter);
-        int indexOfNecessaryLetter = necessaryString.indexOf(necessaryLetter);
-
-
-        System.out.println("Кол-во символов “a” - " + (letterArray.length - 1)+", индексы символа “a” в строке - "+ indexOfNecessaryLetter);
-
+        Scanner inputString = new Scanner(System.in);
+        char necessaryLetter ='a';
+        System.out.println("Вводи строку!");
+        char arrayNumber[] = inputString.nextLine().toCharArray();
+        List<Integer> indexes = new ArrayList<>();
+        for (int i = 0; i < arrayNumber.length; i++){
+            if (arrayNumber[i] == necessaryLetter) {
+                indexes.add(i);
+            }
+        }
+        System.out.println("Кол-во символов “a” - " + indexes.size()+", индексы символа “a” в строке - "+ indexes);
     }
 }
