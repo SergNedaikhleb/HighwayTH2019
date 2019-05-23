bb5e7304f11d8a5b91cdb01173dabb4e4f397dad
s.nedaikhleb <s.nedaikhleb@hexa.com.ua>
2019-05-23 14:52:36 +0300

    login-logout trello

--- src/main/java/lesson11/homeTaskLesson11/TrelloLoginLogout.java
+++ src/main/java/lesson11/homeTaskLesson11/TrelloLoginLogout.java
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit;
 import core.BrowserFactory;
 import trello.pages.LoginPage;
 
-import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.*;
 
 public class TrelloLoginLogout extends BrowserFactory {
 
@@ -18,17 +18,27 @@ public class TrelloLoginLogout extends BrowserFactory {
 
        LoginPage loginPage = new LoginPage();
        loginPage.open();
+       assertNotNull(loginPage);
        assertEquals(driver.getCurrentUrl(), "https://trello.com/login");
        assertEquals("Log in to Trello", driver.findElement(By.tagName("h1")).getText());
-       loginPage.login("sergey.nedaikhleb@gmail.com", "*****");
+       loginPage.login("sergey.nedaikhleb@gmail.com", "");
     }
 
     @Test(priority=2)
     public void checkBoard() {
-      //  assertEquals("Boards", driver.findElement(By.tagName("h3")).getText());
+       // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         assertEquals("Personal Boards", driver.findElement(By.xpath("//*[@id='content']//h3")).getText());
         driver.findElement(By.className("member-initials")).click();
         assertEquals("SergNedaikhleb (sergnedaikhleb)", driver.findElement(By.className("pop-over-header-title")).getText());
+        assertNotSame("Recently Viewed", driver.findElement(By.className("boards-page-board-section-header-name")).getText());
+    }
+
+    @Test(priority=3)
+    public void checkBoardsAmount() {
+        driver.findElement(By.xpath("//*[@id='content']//div[2]//div[2]//div[1]/div/ul/li")).click();
+        assertEquals("Menu", driver.findElement(By.className(".board-menu-header-content")).getText());
+//        driver.navigate().back();
+
     }
 
 
