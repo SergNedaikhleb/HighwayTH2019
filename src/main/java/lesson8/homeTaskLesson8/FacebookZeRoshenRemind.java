2d3613bae2daed30a36c729ae74a8ba04111594a
s.nedaikhleb <s.nedaikhleb@hexa.com.ua>
2019-06-17 15:54:22 +0300

    FB Ze Roshen

--- src/main/java/lesson8/homeTaskLesson8/FacebookZeRoshenRemind.java
+++ src/main/java/lesson8/homeTaskLesson8/FacebookZeRoshenRemind.java
@@ -3,50 +3,66 @@ package lesson8.homeTaskLesson8;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FacebookZeRoshenRemind {
-
     private WebDriver driver;
 
+    private void fbLogin(WebDriver driver) {
+        driver.get("https://facebook.com");
+        driver.findElement(By.id("email")).click();
+        driver.findElement(By.id("email")).sendKeys(login);
+        driver.findElement(By.id("pass")).sendKeys(password);
+        driver.findElement(By.id("loginbutton")).click();
+    }
+
+    private String login = "****@gmail.com";
+    private String password = "********"; // sorry, it's personal
+
+    private static ChromeOptions setChromeOptions() {
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("profile.default_content_setting_values.notifications", 2);
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", prefs);
+        return options;
+    }
+
+    private static String getWordsFromFBFeed(WebDriver driver) {
+
+        JavascriptExecutor jsx = (JavascriptExecutor) driver;
+        for (int i = 0; i < 50; i++)
+            jsx.executeScript("window.scrollBy(0,1000)", "");
+
+        return driver.findElement(By.xpath("//div[contains(@id, 'feed_stream_')]")).getText().toLowerCase();
+    }
+
+
     @BeforeTest
-    public void setUp(){
-        driver = new ChromeDriver();
+    public void setUp() {
+        driver = new ChromeDriver(setChromeOptions());
     }
-
-
-    @Test
-    public void loginFacebook() {
-        driver.manage().window().maximize();
-        driver.get("https://www.facebook.com/");
-        WebDriverWait waitForFBPage = (new WebDriverWait(driver, 8));
-        waitForFBPage.until(ExpectedConditions.presenceOfElementLocated(By.className("mbs _3ma _6n _6s _6v")));
-
-        driver.findElement(By.id("email")).sendKeys("neeshloshad@gmail.com");
-        driver.findElement(By.id("pass")).sendKeys("********"); //sorry, but I can't write you my password
-        WebElement loginButton = driver.findElement(By.id("u_0_3"));
-        loginButton.submit();
-        WebDriverWait waitForMyPage = (new WebDriverWait(driver, 8));
-        waitForMyPage.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='u_0_c']")));
-    }
-    @Test(dependsOnMethods = {"loginFacebook"})
-    public void scrollAndFind() {
-        ((JavascriptExecutor) driver).executeScript("window.scrollBy(0,1000)"); //Scroll vertically down by 1000 pixels
-
-
-    }
-
 
     @AfterTest
-    public void tearDown(){
-        driver.quit();
+    public void testOver() {
+        driver.close();
     }
- }
+
+    @Test
+    public void countRoZe() {
+        if (login.length() == 0 || password.length() == 0) {
+            System.out.println("Empty FB credentials, fill login/password");
+        } else {
+            fbLogin(driver);
+            String feedTxt = getWordsFromFBFeed(driver);
+            System.out.println("Total words in feed: " + (feedTxt.split(" ").length - 1));
+            System.out.println("Mr. Poroshenko mentioned: " + (feedTxt.split("порош").length - 1) + " times");
+            System.out.println("Mr. Zelensky mentioned: " + (feedTxt.split("зеленс").length - 1) + " times");
+        }
+    }
+}
 
 
